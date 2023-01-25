9e8f0b642650c3678b1ceb541e72d16d44171560
ntsmith7 <nick@polarity.io>
2023-01-25 13:14:13 -0800

    chore: Added defaultColor to config files

--- config/config.js
+++ config/config.js
@@ -1,146 +1,146 @@
 module.exports = {
-    /**
-     * Name of the integration which is displayed in the Polarity integrations user interface
-     *
-     * @type String
-     * @required
-     */
-    name: "DiscoverOrg",
-    /**
-     * The acronym that appears in the notification window when information from this integration
-     * is displayed.  Note that the acronym is included as part of each "tag" in the summary information
-     * for the integration.  As a result, it is best to keep it to 4 or less characters.  The casing used
-     * here will be carried forward into the notification window.
-     *
-     * @type String
-     * @required
-     */
-    acronym: "DISC",
-    /**
-     * Description for this integration which is displayed in the Polarity integrations user interface
-     *
-     * @type String
-     * @optional
-     */
-    description: "Company lookup information for DiscoverOrg",
-    entityTypes: ['string', 'domain', 'email'],
-    /**
-     * An array of style files (css or less) that will be included for your integration. Any styles specified in
-     * the below files can be used in your custom template.
-     *
-     * @type Array
-     * @optional
-     */
-    "styles": [
-        "./styles/discover.less"
-    ],
-    /**
-     * Provide custom component logic and template for rendering the integration details block.  If you do not
-     * provide a custom template and/or component then the integration will display data as a table of key value
-     * pairs.
-     *
-     * @type Object
-     * @optional
-     */
-    block: {
-        component: {
-            file: "./components/discover-block.js"
-        },
-        template: {
-            file: "./templates/discover-block.hbs"
-        }
+  /**
+   * Name of the integration which is displayed in the Polarity integrations user interface
+   *
+   * @type String
+   * @required
+   */
+  name: 'DiscoverOrg',
+  /**
+   * The acronym that appears in the notification window when information from this integration
+   * is displayed.  Note that the acronym is included as part of each "tag" in the summary information
+   * for the integration.  As a result, it is best to keep it to 4 or less characters.  The casing used
+   * here will be carried forward into the notification window.
+   *
+   * @type String
+   * @required
+   */
+  acronym: 'DISC',
+  defaultColor: 'light-blue',
+  /**
+   * Description for this integration which is displayed in the Polarity integrations user interface
+   *
+   * @type String
+   * @optional
+   */
+  description: 'Company lookup information for DiscoverOrg',
+  entityTypes: ['string', 'domain', 'email'],
+  /**
+   * An array of style files (css or less) that will be included for your integration. Any styles specified in
+   * the below files can be used in your custom template.
+   *
+   * @type Array
+   * @optional
+   */
+  styles: ['./styles/discover.less'],
+  /**
+   * Provide custom component logic and template for rendering the integration details block.  If you do not
+   * provide a custom template and/or component then the integration will display data as a table of key value
+   * pairs.
+   *
+   * @type Object
+   * @optional
+   */
+  block: {
+    component: {
+      file: './components/discover-block.js'
     },
-    summary: {
-        component: {
-            file: './components/discover-summary.js'
-        },
-        template: {
-            file: './templates/discover-summary.hbs'
-        }
+    template: {
+      file: './templates/discover-block.hbs'
+    }
+  },
+  summary: {
+    component: {
+      file: './components/discover-summary.js'
     },
-    request: {
-        // Provide the path to your certFile. Leave an empty string to ignore this option.
-        // Relative paths are relative to the DISC integration's root directory
-        cert: '',
-        // Provide the path to your private key. Leave an empty string to ignore this option.
-        // Relative paths are relative to the DISC integration's root directory
-        key: '',
-        // Provide the key passphrase if required.  Leave an empty string to ignore this option.
-        // Relative paths are relative to the DISC integration's root directory
-        passphrase: '',
-        // Provide the Certificate Authority. Leave an empty string to ignore this option.
-        // Relative paths are relative to the DISC integration's root directory
-        ca: '',
-        // An HTTP proxy to be used. Supports proxy Auth with Basic Auth, identical to support for
-        // the url parameter (by embedding the auth info in the uri)
-        proxy: '',
-        rejectUnauthorized: true
+    template: {
+      file: './templates/discover-summary.hbs'
+    }
+  },
+  request: {
+    // Provide the path to your certFile. Leave an empty string to ignore this option.
+    // Relative paths are relative to the DISC integration's root directory
+    cert: '',
+    // Provide the path to your private key. Leave an empty string to ignore this option.
+    // Relative paths are relative to the DISC integration's root directory
+    key: '',
+    // Provide the key passphrase if required.  Leave an empty string to ignore this option.
+    // Relative paths are relative to the DISC integration's root directory
+    passphrase: '',
+    // Provide the Certificate Authority. Leave an empty string to ignore this option.
+    // Relative paths are relative to the DISC integration's root directory
+    ca: '',
+    // An HTTP proxy to be used. Supports proxy Auth with Basic Auth, identical to support for
+    // the url parameter (by embedding the auth info in the uri)
+    proxy: '',
+    rejectUnauthorized: true
+  },
+  logging: {
+    level: 'info' //trace, debug, info, warn, error, fatal
+  },
+  /**
+   * Options that are displayed to the user/admin in the Polarity integration user-interface.  Should be structured
+   * as an array of option objects.
+   *
+   * @type Array
+   * @optional
+   */
+  options: [
+    {
+      key: 'apiKey',
+      name: 'DiscoverOrg Partner API Key',
+      description: 'Your DiscoverOrg API Key.',
+      default: '',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
     },
-    logging: {
-        level: 'info',  //trace, debug, info, warn, error, fatal
+    {
+      key: 'username',
+      name: 'DiscoverOrg Username',
+      description: 'Your DiscoverOrg username.',
+      default: '',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
     },
-    /**
-     * Options that are displayed to the user/admin in the Polarity integration user-interface.  Should be structured
-     * as an array of option objects.
-     *
-     * @type Array
-     * @optional
-     */
-    "options": [
-        {
-            "key": "apiKey",
-            "name": "DiscoverOrg Partner API Key",
-            "description": "Your DiscoverOrg API Key.",
-            "default": "",
-            "type": "text",
-            "userCanEdit": true,
-            "adminOnly": false
-        },
-        {
-            "key": "username",
-            "name": "DiscoverOrg Username",
-            "description": "Your DiscoverOrg username.",
-            "default": "",
-            "type": "text",
-            "userCanEdit": true,
-            "adminOnly": false
-        },
-        {
-            "key": "password",
-            "name": "DiscoverOrg Password",
-            "description": "Your DiscoverOrg password.",
-            "default": "",
-            "type": "password",
-            "userCanEdit": true,
-            "adminOnly": false
-        },
-        {
-            "key": "domainBlocklistRegex",
-            "name": "Ignored Domain Regex",
-            "description": "Domains that match the given regex will not be looked up.",
-            "default": "",
-            "type": "text",
-            "userCanEdit": true,
-            "adminOnly": false
-        },
-        {
-            "key": "lookupCompanies",
-            "name": "Company Channels",
-            "description": "list of channel names that represent companies, please note the channel names are case sensitive",
-            "default": "",
-            "type": "text",
-            "userCanEdit": true,
-            "adminOnly": false
-        },
-        {
-            "key": "lookupPeople",
-            "name": "People Channels",
-            "description": "list of people names that represent people you wish to lookup in DiscoverOrg, please note the people names are case sensitive",
-            "default": "",
-            "type": "text",
-            "userCanEdit": true,
-            "adminOnly": false
-        }
-
-    ]
+    {
+      key: 'password',
+      name: 'DiscoverOrg Password',
+      description: 'Your DiscoverOrg password.',
+      default: '',
+      type: 'password',
+      userCanEdit: true,
+      adminOnly: false
+    },
+    {
+      key: 'domainBlocklistRegex',
+      name: 'Ignored Domain Regex',
+      description: 'Domains that match the given regex will not be looked up.',
+      default: '',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
+    },
+    {
+      key: 'lookupCompanies',
+      name: 'Company Channels',
+      description:
+        'list of channel names that represent companies, please note the channel names are case sensitive',
+      default: '',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
+    },
+    {
+      key: 'lookupPeople',
+      name: 'People Channels',
+      description:
+        'list of people names that represent people you wish to lookup in DiscoverOrg, please note the people names are case sensitive',
+      default: '',
+      type: 'text',
+      userCanEdit: true,
+      adminOnly: false
+    }
+  ]
 };
